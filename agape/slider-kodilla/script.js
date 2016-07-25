799f301acf0c40a4113af819016bc42cd534904c
lukgu <szukasz.gutowski@gmail.com>
2016-07-25 11:49:11 +0200

    slide Łukasz

--- agape/slider-kodilla/script.js
+++ agape/slider-kodilla/script.js
@@ -1,6 +1,32 @@
 var carouselList = $("#carousel ul");
+var sumMargin = 0;
+var changeSlide = function() {
+sumMargin =500;
+console.log(sumMargin);
+carouselList.animate({'marginLeft':-sumMargin}, 500, moveFirstSlide);
+}
+function moveFirstSlide(){
+var firstItem = carouselList.find("li:first");
+var lastItem = carouselList.find("li:last");
+$(lastItem.after(firstItem));
+$(carouselList.css({marginLeft:0}));
+
+};
+
+$(function(){
+setInterval(changeSlide, 3000);
+
+
+});
+
+
+
+
+
+
+/*var carouselList = $("#carousel ul");
 var changeSlide = carouselList.animate({'marginLeft':-500}, 500, moveFirstSlide);
-function moveFirstSlide() {
+function moveFirstSlide(){
 	var firstItem = carouselList.find("li:first");
 	var lastItem = carouselList.find("li:last");
 	$(lastItem.after(firstItem));
@@ -9,9 +35,10 @@ function moveFirstSlide() {
 };
 
 $(function(){
-	$('carouselList').setInterval(changeSlide,3000);
+	setInterval(changeSlide, 3000);
 	
 
 });
 
 
+*/
